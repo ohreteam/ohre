6d00dcd9b952599dc2a5c795d69ad836003790d0
koki <k0k1fish@outlook.com>
2025-02-10 21:32:46 +0800

    fix, ModuleVarAnalysis, global var in module start

--- ohre/abcre/dis/lifting/CopyPropagation.py
+++ ohre/abcre/dis/lifting/CopyPropagation.py
@@ -15,12 +15,21 @@ def CopyPropagation(meth: AsmMethod):
         cb.empty_var2val()
     i = 0
     for cb in meth.code_blocks:
-        var2val = CPro_cb(cb)
+        var2val = CPro_cb(cb, DEBUG_MSG=f"CPro_{i}_{len(meth.code_blocks)} Round0")
         cb.set_var2val(var2val)
-        CPro_cb(cb, DEBUG_MSG=f"CPro_{i}_{len(meth.code_blocks)}")  # now var2val in cb updated, CPro it again
+        # now var2val in cb updated, CPro it again
+        CPro_cb(cb, DEBUG_MSG=f"CPro_{i}_{len(meth.code_blocks)} Round1")
         i += 1
     print(f">>> CPro-END {meth.name} {meth._debug_vstr()}")
 
+def var2val_assign(var2val: Dict, var, val):
+    if (isinstance(var, AsmArg) and isinstance(val, AsmArg)):
+        if (val.has_ref() and val.ref_base == var):
+            Log.info(f"var2val_assign: val {val} var {var}, skip")
+        else:
+            var2val[var] = val
+    else:
+        var2val[var] = val
 
 def CPro_cb(cb: CodeBlock, DEBUG_MSG: str = "") -> Dict[AsmArg, AsmArg]:
     print(f"\n\n ===================================== {DEBUG_MSG}")
@@ -28,13 +37,13 @@ def CPro_cb(cb: CodeBlock, DEBUG_MSG: str = "") -> Dict[AsmArg, AsmArg]:
     print(f"\n >>>> CPro_cb START {DEBUG_MSG} cb: {cb._debug_vstr()} var2val {var2val}")
     for i in range(cb.get_insts_len()):
         inst = cb.insts[i]
-        print(f"CPro_cb inst START inst {inst} {inst._debug_vstr()} var2val {var2val}")
+        print(f"CPro_cb inst START {i}/{cb.get_insts_len()} {inst} {inst._debug_vstr()} var2val {var2val}")
         inst.copy_propagation(var2val)
         if (not inst.is_arg0_def()):  # NOTE: skip a inst that not assign any arg (if assigned, must be arg0)
             continue
         if ((inst.is_simple_assgin() and inst.args[0].is_no_ref()) or inst.type == TACTYPE.IMPORT):
-            var2val[inst.args[0]] = inst.args[1]
-        elif (inst.args[0].has_ref() and inst.args[0].is_field()
+            var2val_assign(var2val, inst.args[0], inst.args[1])
+        elif (inst.args[0].has_ref() and (inst.args[0].is_field() or inst.args[0].is_obj())
                 and in_and_not_None(inst.args[0].ref_base, var2val)
                 and var2val[inst.args[0].ref_base].obj_has_key(inst.args[0])):
             print(f"before {var2val[inst.args[0].ref_base]}")
@@ -44,21 +53,21 @@ def CPro_cb(cb: CodeBlock, DEBUG_MSG: str = "") -> Dict[AsmArg, AsmArg]:
             if (inst.args[1].is_imm()):
                 inst.rop = ""
                 inst.args[1].value = - inst.args[1].value
-                var2val[inst.args[0]] = inst.args[1]
+                var2val_assign(var2val, inst.args[0], inst.args[1])
             elif (in_and_not_None(inst.args[1], var2val) and var2val[inst.args[1]].is_imm()):  # a = - b
                 inst.args[1] = copy.deepcopy(var2val[inst.args[1]])
                 inst.args[1].value = - var2val[inst.args[1]].value
-                var2val[inst.args[0]] = inst.args[1]
+                var2val_assign(var2val, inst.args[0], inst.args[1])
             else:
-                var2val[inst.args[0]] = None
+                var2val_assign(var2val, inst.args[0], None)
                 Log.error(f"ERROR-CPro_cb! a = - b else hit, inst {inst}")
         elif (inst.type == TACTYPE.CALL):
             if (inst.args[0] in var2val):
-                var2val[inst.args[0]] = None
+                var2val_assign(var2val, inst.args[0], None)
         else:  # TODO: if hit here, something need to be check, and support it!
             if (inst.args[0] in var2val):
-                var2val[inst.args[0]] = None
+                var2val_assign(var2val, inst.args[0], None)
             Log.error(f"ERROR-CPro_cb! else hit, inst {inst} is_arg0_def {inst.is_arg0_def()} var2val {var2val}")
-        print(f"CPro_cb inst END inst {inst} {inst._debug_vstr()} var2val {var2val}")
+        print(f"CPro_cb inst END {i}/{cb.get_insts_len()} {inst} {inst._debug_vstr()} var2val {var2val}")
     print(f"\n >>>> CPro_cb END {DEBUG_MSG} // var2val {var2val}")
     return var2val
